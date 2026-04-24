84792fd743ed35c69e5c70937f8e5867172ba220
Angel Eduardo Diaz Mendiburu <warydiaz@hotmail.com>
2026-04-24 13:31:04 +0200

    Enhance pagination and search functionality in invoices page

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,13 +11,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set("query", term);
     } else {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 300);
+  }, 300); 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
